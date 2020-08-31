91eed92f9e2832ba3f60a650473c653766c22bee
Anushkakapoor <35295965+Anushkakapoor@users.noreply.github.com>
2020-09-01 01:38:28 +0530

    Update main.py

--- app-engine/main.py
+++ app-engine/main.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START gae_python38_cloudsql_mysql]
+# [START gae_python37_cloudsql_mysql]
 import os
 
 from flask import Flask
@@ -25,52 +25,38 @@ db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
 app = Flask(__name__)
 
-def init():
-        
+
+@app.route('/')
+def main():
     # When deployed to App Engine, the `GAE_ENV` environment variable will be
     # set to `standard`
     if os.environ.get('GAE_ENV') == 'standard':
-        # If deployed, use the local socket interface for accessing Cloud SQL
-        unix_socket = '/cloudsql/{}'.format(db_connection_name)
-        cnx = pymysql.connect(user=db_user, password=db_password,
-                              unix_socket=unix_socket, db=db_name)
-    else:
         # If running locally, use the TCP connections instead
         # Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
         # so that your application can use 127.0.0.1:3306 to connect to your
         # Cloud SQL instance
-        host = '127.0.0.1'
+        host = '172.23.16.3'
         cnx = pymysql.connect(user=db_user, password=db_password,
                               host=host, db=db_name)
-        return cnx
-@app.route('/')       
-def main():
-    cnx=init()    
+    else:
+        # If deployed, use the local socket interface for accessing Cloud SQL
+        unix_socket = '/cloudsql/{}'.format(db_connection_name)
+        cnx = pymysql.connect(user=db_user, password=db_password,
+                              unix_socket=unix_socket, db=db_name)
+
     with cnx.cursor() as cursor:
         cursor.execute('select * from transactions;')
         result = cursor.fetchall()
-        current_msg = result
-    cnx.close()
-
-    return str(current_msg)
-# [END gae_python37_cloudsql_mysql]
-
-@app.route('/transactionByUserId',methods=['POST'])
-def get_user_transaction():
-    cnx=init()
-    if request.method == 'POST':
-        user_id = flask.request.form.get('user_id')
-            
-    with cnx.cursor() as cursor:
-        cursor.execute('select * from transactions where customer_id=' + user_id +';')
-        result = cursor.fetchall()
-        current_msg = result
+        current_msg = result[0][0]
     cnx.close()
 
     return flask.jsonify(current_msg)
 # [END gae_python37_cloudsql_mysql]
 
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+
+
 
     
