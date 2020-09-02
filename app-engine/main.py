bef393fe77c56f0d8afa295c5bcfe1fc9f31fc1c
Anushkakapoor <35295965+Anushkakapoor@users.noreply.github.com>
2020-09-03 00:31:32 +0530

    Update main.py

--- app-engine/main.py
+++ app-engine/main.py
@@ -64,6 +64,37 @@ def get_user_transaction():
 
     return str(current_msg)
 
+@app.route('/balanceByUserId',methods=['POST'])
+
+def get_user_transaction():
+    # When deployed to App Engine, the `GAE_ENV` environment variable will be
+    # set to `standard`
+    if os.environ.get('GAE_ENV') == 'standard':
+        # If deployed, use the local socket interface for accessing Cloud SQL
+        unix_socket = '/cloudsql/{}'.format(db_connection_name)
+        cnx = pymysql.connect(user=db_user, password=db_password,
+                              unix_socket=unix_socket, db=db_name)
+    else:
+        # If running locally, use the TCP connections instead
+        # Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
+        # so that your application can use 127.0.0.1:3306 to connect to your
+        # Cloud SQL instance
+        host = '127.0.0.1'
+        cnx = pymysql.connect(user=db_user, password=db_password,
+                              host=host, db=db_name)
+        
+    if request.method == 'POST':
+        user_id = request.form.get('user_id')
+
+
+    with cnx.cursor() as cursor:
+        cursor.execute('select closing_balance from transactions where customer_id=' + user_id +';')
+        result = cursor.fetchall()
+        current_msg = result
+    cnx.close()
+
+    return str(current_msg)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
